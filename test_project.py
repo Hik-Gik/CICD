b3bd9a7e07ecade79bf31e7e57073c0160de3e83
Hik-Gik <109528934+Hik-Gik@users.noreply.github.com>
2024-10-23 20:57:45 +0300

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,7 +1,12 @@
 import pytest
-from project import add
+from project import add, multiply
 
 def test_add():
     assert add(1, 2) == 3
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 1) == 0
+    assert multiply(-1, 2) == -2
